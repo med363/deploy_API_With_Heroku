0ed462ec70e6f97d64e449857157f3db4496d8be
med363 <blibechmedamine@gmail.com>
2023-07-12 11:57:52 +0100

    design listview

--- lib/product_item.dart
+++ lib/product_item.dart
@@ -16,8 +16,79 @@ class ProductItem  extends StatelessWidget {
           color: Colors.white,
           borderRadius: BorderRadius.circular(50),
         ),
-        child: Text(model!.productName ?? ""),
+        child: productWidget(context),
       ),
+    );
+  }
+  Widget productWidget(context){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 120,
+          alignment: Alignment.center,
+          margin: const EdgeInsets.all(10),
+          child: Image.network(
+            (model!.productImage == null || model!.productImage == "") 
+            ? "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+            : model!.productImage!,
+            height: 70,
+            fit: BoxFit.scaleDown,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                model!.productName!,
+                style: const TextStyle(
+                  color: Color.fromARGB(66, 50, 80, 40),
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "${model!.productPrice}",
+                style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width - 180,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      child: const Icon(
+                        Icons.edit
+                      ),
+                      onTap: () {},
+                      ),
+                    GestureDetector(
+                      child: const Icon(
+                        Icons.delete,
+                        color: Color.fromARGB(200, 180, 120, 120),
+                      ),
+                      onTap: () {
+                        onDelete!(model);
+                      },
+                      )
+
+                  ],
+                ),
+              )
+            ],
+          ),
+
+        )
+      ],
     );
   }
 }
