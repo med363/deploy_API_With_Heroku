8d7556f0ccc7c7e8dff1b037dc162624e898d34e
med363 <blibechmedamine@gmail.com>
2023-07-13 11:57:22 +0100

    widget for image and two botton to upload image on wich src

--- lib/pages/product_add_edit.dart
+++ lib/pages/product_add_edit.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 import 'package:snippet_coder_utils/FormHelper.dart';
+import 'package:snippet_coder_utils/hex_color.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProductAddEdit extends StatefulWidget {
   const ProductAddEdit({super.key});
@@ -89,13 +93,102 @@ Widget productForm() {
               showPrefixIcon: false,
               suffixIcon: const Icon(Icons.monetization_on),
             ),
-          )
+          ),
+          //appel de widget
+          picPicker(false,"",(file){}),
+          const SizedBox(
+            height: 20,
+          ),
+          //cree submit button
+          Center(child: FormHelper.submitButton("save",() {},
+          btnColor: HexColor("#283B71"),
+          borderColor: Colors.white,
+          borderRadius: 10),)
         ],
       )
     );
 }
 
 //widget for image and two botton to upload image on wich src
+ static Widget picPicker(
+  bool isFileSelected,
+  String filleName,
+  Function onFilePicked,
+ ) {
+  Future<XFile?> _imageFile;
+  ImagePicker _picker = ImagePicker();
+  return Column(
+    children: [
+      filleName.isNotEmpty 
+      ?isFileSelected
+       ? Image.file(File(filleName), 
+       height: 200,
+       width: 200,
+       )
+      : SizedBox(
+        child: Image.network(
+          filleName,
+           width: 200,
+            height: 200,
+             fit: BoxFit.scaleDown,
+             ),
+             )
+      : SizedBox(
+        child: Image.network(
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png",
+           width: 200,
+            height: 200,
+             fit: BoxFit.scaleDown,
+             ),
+             ),
+             //two button on button for glery and other for camera
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 35.0,
+                  width: 35.0,
+                  child: IconButton(
+                    padding: const EdgeInsets.all(0),
+                    icon: const Icon(
+                      Icons.image,
+                      size: 35,
+                    ) ,
+                    onPressed: (){
+                      //action of button
+                      _imageFile = _picker.pickImage(source: ImageSource.gallery);
+                      _imageFile.then((File) async {
+                        onFilePicked(File);
+                      });
+                    },
+                    ),
+                ),
+                //other button
+                 SizedBox(
+                  height: 35.0,
+                  width: 35.0,
+                  child: IconButton(
+                    padding: const EdgeInsets.all(0),
+                    icon: const Icon(
+                      Icons.camera,
+                      size: 35,
+                    ) ,
+                    onPressed: () {
+                      _imageFile = _picker.pickImage(source: ImageSource.camera);
+                      _imageFile.then((File) async {
+                        onFilePicked(File);
+                      });
+                    },
+                    ),
+                )
+
+              ],
+
+            ) 
+
+    ],
+  );
+ }
           
     // Widget productForm(){
   //   return SingleChildScrollView(
